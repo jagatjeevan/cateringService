096426e8fc9df369a02a4664a886828d0ca14a40
Vivek Kumar <kvivek@thoughtworks.com>
2018-09-03 11:10:14 +0530

    Vivek | add

--- Apis/app.py
+++ Apis/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def home():
-    return "Hello, world"
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
 
+
+api.add_resource(Student, '/student/<string: name>')
 
 app.run(port=5000)
